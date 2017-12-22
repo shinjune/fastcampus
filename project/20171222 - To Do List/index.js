d01026ab144824569e7cdbff8f8de39beef6815e
June Shin <shinjune2@gmail.com>
2017-12-22 17:01:37 +0900

    test update

--- project/20171222 - To Do List/index.js
+++ project/20171222 - To Do List/index.js
@@ -3,8 +3,26 @@ const inputEl = document.querySelector('#todo-input');
 const listEl = document.querySelector('#todo-list')
 
 addButtonEl.addEventListener('click', e => {
-  const itemEl = document.createElement('div')
-  itemEl.textContent = inputEl.value;
-  listEl.appendChild(itemEl);
-  inputEl.value = ""
-})
+    const itemEl = document.createElement('div')
+    itemEl.textContent = inputEl.value;
+    listEl.appendChild(itemEl);
+    inputEl.value = ""
+
+    itemEl.addEventListener('click', e => {
+      if (itemEl.classList.contains('complete')) {
+        itemEl.classList.remove('complete')
+      } else {
+        itemEl.classList.add('complete');
+      }
+    })
+
+    const removeButtonEl = document.createElement('div')
+    itemEl.appendChild(removeButtonEl)
+    removeButtonEl.textContent = 'x'
+
+    removeButtonEl.addEventListener('click', e => {
+      listEl.removeChild(itemEl)
+
+    })
+  })
+  //click시 delete div
